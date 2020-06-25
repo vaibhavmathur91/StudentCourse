1938f2cbbb177d7bfdee45a92e52829f156a654b
vaibhavmathur91 <vaibhav@hiringjunction.com>
2020-06-26 00:51:59 +0530

    added index page function

--- root/StudentWeb/models.py
+++ root/StudentWeb/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    contact_number = models.CharField(max_length=15)
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    course_details = models.CharField(max_length=200)
+
+
+class StudentCourses(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
--- root/StudentWeb/views.py
+++ root/StudentWeb/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Student, Course, StudentCourses
 
-# Create your views here.
+
+def get_all_students():
+    return Student.objects.all()
+
+
+def index(request):
+    students = get_all_students()
+    context = {
+        'students_list': students,
+    }
+    return render(request, 'StudentWeb/index.html', context)
+
+
+def add_student(request):
+    f_name = request.POST("first_name")
+    l_name = request.POST("last_name")
+    dob = request.POST("date_of_birth")
+    phone = request.POST("number")
+
+    student = Student(
+        first_name=f_name,
+        last_name=l_name,
+        birth_date=dob,
+        contact_number=phone
+    ).save()
+
+    return get_all_students()
