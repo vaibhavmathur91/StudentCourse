a5e93472103fbbba1f4033753f6c7301e7dc601a
vaibhavmathur91 <vaibhav@hiringjunction.com>
2020-06-26 01:35:27 +0530

    added student_insert method and index page

--- root/StudentWeb/urls.py
+++ root/StudentWeb/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_student', TemplateView.as_view(template_name='StudentWeb/add_student.html'), name='add_student'),
+    path('/post_add_student', views.add_student, name='post_add_student'),
 ]
--- root/StudentWeb/views.py
+++ root/StudentWeb/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Student, Course, StudentCourses
 
 
@@ -16,16 +17,15 @@ def index(request):
 
 
 def add_student(request):
-    f_name = request.POST("first_name")
-    l_name = request.POST("last_name")
-    dob = request.POST("date_of_birth")
-    phone = request.POST("number")
-
-    student = Student(
-        first_name=f_name,
-        last_name=l_name,
-        birth_date=dob,
-        contact_number=phone
-    ).save()
-
-    return get_all_students()
+    f_name = request.POST["first_name"]
+    l_name = request.POST["last_name"]
+    dob = request.POST["date_of_birth"]
+    phone = request.POST["number"]
+    print(request, request.POST)
+    # Student(
+    #     first_name=f_name,
+    #     last_name=l_name,
+    #     birth_date=dob,
+    #     contact_number=phone
+    # ).save()
+    return HttpResponseRedirect("/")
